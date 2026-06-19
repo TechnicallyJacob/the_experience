27f7de84529f30aeb863c5551dcc1b334fdd5765
TechnicallyJacob <158979849+TechnicallyJacob@users.noreply.github.com>
2026-06-19 17:14:24 -0500

    Add files via upload

---
+++ NOTES.md
@@ -0,0 +1,11 @@
+skill tree
+
+Melee -
+    (handheld weapons.. swords, daggers, axes)
+
+Marksman - 
+    (ranged weapons.. bow, slingshot, guns, crossbow, spear, shruiken)
+
+Brawling - 
+    (just body melee.. punches, kicks, headbutt, buttsmash)
+
---
+++ funcs/intro.py
@@ -0,0 +1,9 @@
+player = {}
+
+print("Hello traveler! What is your name so we can get this adventure started!?");
+player_name = input();
+player["name"] = player_name;
+print('Ahhh ' + player["name"] + ', I once knew a ' + player["name"] + ", unfortunately they took an arrow to the knee and died from syphilis.");
+print("Hahaha, nevermind that, what is your ")
+
+print(player);
