bf8813e505684243bacd29414757a14642148475
Jacob Chanders <JTLanders042718@gmail.com>
2026-06-22 17:55:16 -0500

    testing pygame

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import time
-
+import pygame
 from funcs import intro, town, forest
-
 from objs.player_object import player
 
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
 user_input = ""
 
-while True:
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+    screen.fill("purple")
 
     intro
 
     user_input = input("What would you like to do? \nChoices = 'gold', 'inventory', 'stats', 'town', 'forest', 'end' \n").strip().lower()
 
-    while not True:
+    while True:
         if user_input.strip().lower() == "gold":
             print(player["gold"])
             time.sleep(0.5)
@@ -63,7 +73,15 @@ while True:
             break
     if user_input.strip().lower() == "end":
         player["health"] = 0
+        running = False
+        pygame.quit()
+
+    pygame.display.flip()
+
+    clock.tick(60)
 
     if player["health"] == 0:
         print(f"Good luck next time {player['name']}")
+        running = False
+        pygame.quit()
         break
